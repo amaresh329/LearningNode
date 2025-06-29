44f986c401f5ee9773fac6e3643b28ca86dc9fb2
amaresh329 <vutukuriamaresh930@gmail.com>
2025-06-29 21:56:52 +0530

    Learn how v8 engine works when we give our code

---
+++ v8engine.js
@@ -0,0 +1,26 @@
+//1.what is v8 engine?
+//2.how ypur code is transformed in to v8 engine and how it works?
+//3.how the compiler and interpreter works in v8 engine?
+//4.how the code is parsed into v8 engine and the steps of conversion
+
+//learned the v8 engine how it works and done deep dive into node.js
+//types of garbage collector
+/*
+compileres in v8
+1.turbo fan compiler
+interpreter in v8
+1.ignition interpreter
+
+garbage collector in v8
+1.orinoco
+2.oil pan
+3.scavenges
+
+*/
+
+/*
+Steps how code works in v8
+1.parsing stage:Lexical Analysis and Tokenization
+2.Syntax analysis and  Abstract syntax tree(AST)
+3.HOT code optimization and Deoptimization
+*/
