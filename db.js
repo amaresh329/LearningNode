9d11dcb16a6f334b71b6b91ea292567a8d61af06
amaresh329 <vutukuriamaresh930@gmail.com>
2025-07-07 22:15:55 +0530

    database history and SQL vs NoSQL

---
+++ db.js
@@ -0,0 +1,16 @@
+/*
+1.what is database?
+2.Types of Database
+3.what is DBMS?
+4.Different typesof Databases
+5.SQL vs NoSQL
+6..what is relational databases and how it works?
+7.History of SQl and who invented it
+8.History of MYSQL and who invented it and how it is named as MYSql(Michael widenir)
+9.History of Progress Sql and who invented it and how it is named as postgress.(Michael stone Breaker)
+10.Which company first took the MYSQL(Sun microsystems)
+11.How mongodb came and gistory of mongodb and how it is named as mongo(humongous)
+12.which companyb manages the mongodb and how that company changed to MOngodb inc from another comapny name
+13.EF codd(Founder of RDB) and chodds 12 rule and if anyone uses this 12rules(0-12) then it can become a relational db
+14.mongodb created by 10gen company and later changed it to mongodb inc.
+*/
