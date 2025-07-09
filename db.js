e0effd9272d429d7845319d9b41dfd2ca9f23ad7
amaresh329 <vutukuriamaresh930@gmail.com>
2025-07-09 12:32:56 +0530

    connected mongodb with node and done the mongodb documentation

--- db.js
+++ db.js
@@ -15,8 +15,49 @@
 14.mongodb created by 10gen company and later changed it to mongodb inc.
 */
 
-//Go to mongodb website
-//create a clustrer
-//create a username and password
-//Get the cluster
-const URI="mongodb+srv://abcd:abcd@cluster0.rfnapzt.mongodb.net/"
+const { MongoClient } = require('mongodb');
+const url="mongodb+srv://abcd:abcd1234@cluster0.rfnapzt.mongodb.net/";
+const client=new MongoClient(url);
+const dbname='Helloworld';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbname);
+  const collection = db.collection('user');
+
+  // the following code examples can be pasted here...
+
+  const data={
+    "first name":"Rajesh",
+    "last name":"Bandharu",
+    "City":"Eskota",
+    "phone number":"7438889763"
+
+  }
+  const insertResult = await collection.insertMany([data]);
+  console.log('Inserted documents =>', insertResult);
+   
+  const findResult = await collection.find({}).toArray();
+  console.log('Found documents =>', findResult);
+
+  return 'done.';
+}
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
+/*
+*Notes
+*Go to mongodb website
+*create a clustrer
+*create a username and password
+*Get the cluster
+*Install mogodb compass
+*To connect our mongodb to the node we need to use one npm package i.e; npm i mongodb
+*do the process of connecting to mongodb from mongodb documentation
+*Do crud operations
+
+*/
