a936c9dc1284ae4580959163175ec94eab9621b4
amaresh329 <vutukuriamaresh930@gmail.com>
2025-07-18 12:25:04 +0530

    learn about monolith vs microservices and SDLC in real world projects

---
+++ projectintro.js
@@ -0,0 +1,18 @@
+/**
+ * Requirements
+ * Design
+ * Development
+ * Testing
+ * Deployment
+ * Maintainance
+ This the waterfall model (software Development life cycle) used by companies for developing a project
+
+ *Monolith vs Microservices
+
+ whta happens in namastedev.com?
+ student web     admin web   backend     student app in mobile
+ next.js          react.js    node.js     react native
+
+ what are we doing in dev tinder?
+ frontend   backend
+ */
