0236ce4404ede0027bb6fb15f2782830374b7f7f
amaresh329 <vutukuriamaresh930@gmail.com>
2025-07-20 19:51:07 +0530

    HLD,LLD,REST API's theory and features of Dev TINDER

---
+++ projectstructure.js
@@ -0,0 +1,37 @@
+/*
+Features,HLD & LLD
+Dev Tinder(features)
+*create an account
+*login
+*update your profile
+*Feedpage
+*send connection request
+*see our matches
+*see the request we've sent/received 
+*update your profile
+
+HLD
+*front end,backend
+*frontend-react
+*backend-node
+*database:mongodb
+
+LLD
+*DB design
+*REST API's
+
+Learnt about rest APIS a and methods of rest apis
+APIS infra
+*POST/signup 
+*POST/Login
+*GET/profile 
+*POST/profile
+*PATCH/profile 
+*Delete/Profile 
+*POST/Send Request
+*POST/review my pending request
+*GET/requests 
+*GET/connections
+
+
+*/
